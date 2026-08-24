848a110f475282b6d20e5e54b9e415f473311830
wazazhang <wazazhang@gmail.com>
2026-08-24 19:02:15 +0800

    Scope

--- DeepMetaGame.CLI/Program.cs
+++ DeepMetaGame.CLI/Program.cs
@@ -4,8 +4,20 @@ class Program
     [STAThread]
     static int Main(string[] args)
     {
+        if (args.Length > 0)
+        {
+            var cmd = args[0];
 
+            if (cmd == "gen")
+            {
 
+            }
+        }
         return 0;
+    }
+
+    static void gen(string[] args)
+    {
+
     }
 }
