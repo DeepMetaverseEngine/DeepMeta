43bac0c8834c167a5cdb177a3f133a7b1a29908f
wazazhang <wazazhang@gmail.com>
2026-08-24 22:02:37 +0800

    aaa

--- DeepMetaGame.CLI/Program.cs
+++ DeepMetaGame.CLI/Program.cs
@@ -4,10 +4,9 @@ class Program
     [STAThread]
     static int Main(string[] args)
     {
-        if (args.Length > 0)
+        if (args.Length > 1)
         {
             var cmd = args[0];
-
             if (cmd == "gen")
             {
 
@@ -16,8 +15,10 @@ class Program
         return 0;
     }
 
-    static void gen(string[] args)
+    static void gen(string[] args, DirectoryInfo root)
     {
+        var projName = root.Name;
+
 
     }
 }
