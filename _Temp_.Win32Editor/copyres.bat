d7b05a0650c512e8d2208a070159bcbc0e307f1e
wazazhang <wazazhang@gmail.com>
2026-08-25 22:17:38 +0800

    _Temp_

--- _Temp_.Win32Editor/copyres.bat
+++ _Temp_.Win32Editor/copyres.bat
@@ -2,8 +2,8 @@
 
 SET TargetDir=%1
 
-rem md %~dp0..\..\..\GameEditor\bin
+md %~dp0..\..\..\GameEditor\bin
 
-rem xcopy /Y/E  %TargetDir%\*   %~dp0..\..\..\GameEditor\bin
+xcopy /Y/E  %TargetDir%\*   %~dp0..\..\..\GameEditor\bin
 
 exit 0
