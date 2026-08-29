1eba07aec0b94da13bd94a8d1866c3840fed48fd
wazazhang <wazazhang@gmail.com>
2026-08-29 16:03:54 +0800

    m

---
+++ DeepCore.Net/HttpClient/HttpClientFactory.cs
@@ -0,0 +1,30 @@
+﻿using DeepCore.IO;
+using DeepCore.NetClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeepCore.Net.HttpClient
+{
+        public class HttpClientFactory : NetClientFactory
+        {
+            static public HttpClientFactory HttpInstance { get; private set; } = new HttpClientFactory();
+            public HttpClientFactory()
+            {
+            HttpInstance = this;
+            }
+
+            public static Dictionary<string, string> ClientHeader { get; set; }
+            public static List<string> SubProtocol { get; set; }
+
+            public virtual WSNetClient CreateClient(IExternalizableFactory codec, string name = null, int request_timer_tick_ms = 5000)
+            {
+                return new WSNetClient(codec, name, request_timer_tick_ms);
+            }
+            public override IClientAdapter CreateAdapter(INetClient client)
+            {
+                return new WSWebSocketAdapter(client);
+            }
+        }
+    
+}
